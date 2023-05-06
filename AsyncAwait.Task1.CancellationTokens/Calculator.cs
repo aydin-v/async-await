6d28da5bcc4c8bae577ebef7cb4d7d7ad6d95e99
Aydin Valiyev <aydin0294@gmail.com>
2023-05-06 22:12:22 +0200

    Initial commit

---
+++ AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens;
+
+internal static class Calculator
+{
+    public async static Task<long> CalculateAsync(int n, CancellationToken token)
+    {
+        return await Task
+                .Run(() =>
+                {
+                    long sum = 0;
+                    for (var i = 0; i < n; i++)
+                    {
+                        token.ThrowIfCancellationRequested();
+
+                        sum += i + 1;
+                        Thread.Sleep(10);
+                    }
+                    return sum;
+                },token);
+    }
+}
